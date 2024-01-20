771ca6a31aeed7d874872bddb692a20d3a5c98ab
ArchLance <lancee_zhang@163.com>
2024-01-21 00:07:40 +0800

    [lab3]: finsih phase4
    Signed-off-by: ArchLance <lancee_zhang@163.com>

---
+++ lab3/target1/phase3/phase3.d
@@ -0,0 +1,10 @@
+
+phase3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	bf b8 dc 61 55       	mov    $0x5561dcb8,%edi
+   5:	68 fa 18 40 00       	pushq  $0x4018fa
+   a:	c3                   	retq   
---
+++ lab3/target1/phase3/phase3.s
@@ -0,0 +1,3 @@
+mov $0x5561dcb8,%edi
+push $0x4018fa
+retq
